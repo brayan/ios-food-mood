2560ca051c3f7e12217b2abfa2f7ea38a597db15
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2019-04-04 17:46:24 -0400

    Initial Commit

---
+++ FoodMood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodMood
+//
+//  Created by user153528 on 4/4/19.
+//  Copyright © 2019 user153528. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
